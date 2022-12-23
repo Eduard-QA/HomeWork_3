8e9bbfe38ed5fc0ccb188e0d57eec608b7ae9cb5
Eduard-QA <eduard.mickel@gmail.com>
2022-12-24 00:19:19 +0200

    HW3

---
+++ src/test/java/HomeWork_3.java
@@ -0,0 +1,38 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class HomeWork_3 {
+
+    WebDriver wd;
+
+    @BeforeMethod
+    public void preCondition() {
+        wd = new ChromeDriver();
+
+    }
+
+    @Test
+    public void loginPhonebook() {
+        wd.navigate().to("https://telranedu.web.app/");
+        wd.findElement(By.cssSelector("[href = '/login'")).click();
+        WebElement email = wd.findElement(By.cssSelector("[placeholder='Email']"));
+        email.click();
+        email.clear();
+        email.sendKeys("rehovot15@mail.ru");
+        WebElement password = wd.findElement(By.cssSelector("[placeholder='Password']"));
+        password.click();
+        password.clear();
+        password.sendKeys("Remember15@");
+        //wd.findElement(By.cssSelector("[name$='registration']")).click();
+        wd.findElement(By.cssSelector("[type$='submit']")).click();
+       // wd.findElement(By.cssSelector("button [contains(text(),'Sign Out' "));
+       // signOut.click();
+
+    }
+
+
+}
